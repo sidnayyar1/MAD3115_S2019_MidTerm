85b3e862e4c5e60f7c34f79603b99e48b53afc5a
sidharth nayyar <sidnayyar1@gmail.com>
2019-07-11 19:03:44 -0400

    [sidharth] initial commit before coding.

---
+++ C0752828_MidTerm_MAD3115S2019/C0752828_MidTerm_MAD3115S2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0752828_MidTerm_MAD3115S2019
+//
+//  Created by Sidharth Nayyar on 2019-07-11.
+//  Copyright © 2019 Sidharth Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
