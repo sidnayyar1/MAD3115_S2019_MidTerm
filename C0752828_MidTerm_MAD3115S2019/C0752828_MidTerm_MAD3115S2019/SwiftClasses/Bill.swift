d474218af1291aeb942c1c3fcb0ab726740efb13
sidharth nayyar <sidnayyar1@gmail.com>
2019-07-12 16:01:36 -0400

    [sidharth]created login screen and used remember Me switch. user default memory saved the email and password for the timming application is running to save data.

---
+++ C0752828_MidTerm_MAD3115S2019/C0752828_MidTerm_MAD3115S2019/SwiftClasses/Bill.swift
@@ -0,0 +1,28 @@
+//
+//  Bill.swift
+//  C0752828_MidTerm_MAD3115S2019
+//
+//  Created by Sidharth Nayyar on 2019-07-12.
+//  Copyright © 2019 Sidharth Nayyar. All rights reserved.
+//
+
+import Foundation
+class Bill{
+
+
+    var billId: Int?
+    var bill_Date: Date?
+    var bill_Type: String?
+    var total_Bill_Amount: Float?
+
+    init(bill_ID:Int,billDate:Date,billType:String,totalBillAmount:Float) {
+
+        self.billId = bill_ID
+        self.bill_Date = billDate
+        self.bill_Type = billType
+        self.total_Bill_Amount = totalBillAmount
+
+
+    }
+
+}
