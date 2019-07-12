dab4ac0ea3c21e06c468203627cd457f197210ea
sidharth nayyar <sidnayyar1@gmail.com>
2019-07-12 06:22:17 -0400

    [Sidharth] created BillListTableViewController 		using PLIST to validate the user for correct email and password

---
+++ C0752828_MidTerm_MAD3115S2019/C0752828_MidTerm_MAD3115S2019/BillListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillListTableViewController.swift
+//  C0752828_MidTerm_MAD3115S2019
+//
+//  Created by Sidharth Nayyar on 2019-07-12.
+//  Copyright © 2019 Sidharth Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class BillListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- C0752828_MidTerm_MAD3115S2019/C0752828_MidTerm_MAD3115S2019/loginViewController.swift
+++ C0752828_MidTerm_MAD3115S2019/C0752828_MidTerm_MAD3115S2019/loginViewController.swift
@@ -8,20 +8,53 @@
 
 import UIKit
 
-class loginViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var userEmailTxt: UITextField!
     @IBOutlet weak var userPasswordTxt: UITextField!
     @IBOutlet weak var rememberMeSwitch: UISwitch!
+  
+    var loggedIn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
     @IBAction func loginBtn(_ sender: UIButton) {
         
+        var email = self.userEmailTxt.text!
+        var Password = self.userPasswordTxt.text!
         
+        if let pList = Bundle.main.path(forResource: "userInfo", ofType: "plist"){
+        
+        if let dict = NSDictionary(contentsOfFile: pList) {
+          
+            if let users = dict["userList"] as? [[String:Any]]{
+                
+                for user in users{
+                    if ( (email == (user["userEmailId"] as! String)) && (Password == (user["userPassword"] as! String)) ){
+                        print("Hello")
+                        let sb = UIStoryboard(name: "Main", bundle: nil)
+                        
+                       self.performSegue(withIdentifier: "userProfile", sender: nil)
+                       loggedIn = true
+                    }
+                    
+                        }
+                    }
+                }
+            }
+        
+        if !loggedIn{
+            let alert = UIAlertController(title: "Error", message: "User Email ID or Password is Incorrect", preferredStyle: UIAlertController.Style.alert)
+            let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil)
+             let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(okAction)
+            alert.addAction(cancelAction)
+            self.present(alert,animated: true ,completion: nil)
+        }
+        
+        }
     }
-    
-}
-
